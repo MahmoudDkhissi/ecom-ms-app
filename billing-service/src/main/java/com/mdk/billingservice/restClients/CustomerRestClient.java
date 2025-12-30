4288dad0fb8f226203bb6031c7ebaf186f3e892f
Mahmoud Dkhissi <mahmouddkhissi68@gmail.com>
2025-12-31 00:47:22 +0100

    ajout de circuit breakers sur les deux restclients

--- billing-service/src/main/java/com/mdk/billingservice/restClients/CustomerRestClient.java
+++ billing-service/src/main/java/com/mdk/billingservice/restClients/CustomerRestClient.java
@@ -2,6 +2,7 @@ package com.mdk.billingservice.restClients;
 
 
 import com.mdk.billingservice.models.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,5 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "customer-service")
 public interface CustomerRestClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customer-cb", fallbackMethod = "getDefaultCustomer")
     Customer findById(@PathVariable Long id);
+
+    default Customer getDefaultCustomer(Long id, Exception ignoredE) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setName("default");
+        customer.setEmail("default@gmail.com");
+        return customer;
+    }
 }
--- billing-service/src/main/java/com/mdk/billingservice/restClients/ProductRestClient.java
+++ billing-service/src/main/java/com/mdk/billingservice/restClients/ProductRestClient.java
@@ -2,6 +2,7 @@ package com.mdk.billingservice.restClients;
 
 
 import com.mdk.billingservice.models.Product;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ProductRestClient {
 
     @GetMapping("/products/{id}")
+    @CircuitBreaker(name = "product-cb", fallbackMethod = "getDefaultProduct")
     Product findById(@PathVariable Long id);
+
+    default Product getDefaultProduct(Long id, Exception ignoredE) {
+        Product product = new Product();
+        product.setId(id);
+        product.setName("default product");
+        return product;
+    }
 
 }
