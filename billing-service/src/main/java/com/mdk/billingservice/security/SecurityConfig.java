5db3b30a55d82805b454a91c1ce24a47d6332911
Mahmoud Dkhissi <mahmouddkhissi68@gmail.com>
2026-01-20 23:47:38 +0100

    enable OAuth2 JWT authentication in services and add SecurityConfig classes

---
+++ billing-service/src/main/java/com/mdk/billingservice/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.mdk.billingservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/actuator/**").permitAll())
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/h2-console/**").permitAll())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
